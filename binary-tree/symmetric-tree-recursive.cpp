fed9c29cf65cdc47f35a7951d39c4979ad5e38d6
nyctophiliacme <pransh7575@gmail.com>
2018-07-05 15:44:47 +0530

    Binary Tree started

---
+++ binary-tree/symmetric-tree-recursive.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool func(TreeNode* root1, TreeNode* root2)
+    {
+        if(root1 == NULL && root2 == NULL)
+        {
+            return true;
+        }
+        if(root1 == NULL || root2 == NULL)
+        {
+            return false;
+        }
+        if(root1->val != root2->val)
+        {
+            return false;
+        }
+        return (func(root1->left, root2->right) && func(root1->right, root2->left));
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL)
+            return true;
+        return func(root->left, root->right);
+    }
+};
